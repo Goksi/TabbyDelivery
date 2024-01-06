581d2b3d9339816939d74c6dfb7082ee9f4395b8
Vukašin Lekić <goksi1312@gmail.com>
2024-01-06 04:23:23 +0100

    panama ko beograd nananana

--- src/main/java/tech/goksi/projekatop/controllers/nalog/PodesavanjaController.java
+++ src/main/java/tech/goksi/projekatop/controllers/nalog/PodesavanjaController.java
@@ -1,7 +1,10 @@
 package tech.goksi.projekatop.controllers.nalog;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.KeyEvent;
 import tech.goksi.projekatop.models.Korisnik;
 import tech.goksi.projekatop.models.KorisnikInjectable;
 import tech.goksi.projekatop.persistance.DataStorage;
@@ -9,12 +12,20 @@ import tech.goksi.projekatop.persistance.DataStorageInjectable;
 
 public class PodesavanjaController implements DataStorageInjectable, KorisnikInjectable {
     @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField repeatedPasswordField;
+    @FXML
+    private Label errorLabel;
+    @FXML
     private Label infoLabel;
     private Korisnik currentUser;
     private DataStorage storage;
 
     public void initialize() {
-        infoLabel.setText(String.format(infoLabel.getText(), currentUser.getUsername(), currentUser.isAdmin() ? "DA" : "NE", 1)); // TODO
+        infoLabel.setText(String.format(
+                infoLabel.getText(), currentUser.getUsername(), currentUser.isAdmin() ? "DA" : "NE", currentUser.getDatumRegistracije(), 1
+        )); // TODO
     }
 
     @Override
@@ -25,5 +36,11 @@ public class PodesavanjaController implements DataStorageInjectable, KorisnikInj
     @Override
     public void setDataStorage(DataStorage storage) {
         this.storage = storage;
+    }
+
+    public void onPromeniLoznikuClick(ActionEvent actionEvent) {
+    }
+
+    public void onFieldWrite(KeyEvent keyEvent) {
     }
 }

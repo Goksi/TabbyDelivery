10cfdb6a530e5961ceb55098671dbbd5d5925605
Vukašin Lekić <goksi1312@gmail.com>
2024-01-06 15:59:34 +0100

    Samo cu ga "ispeglati" ovako umesto join-a

--- src/main/java/tech/goksi/projekatop/persistance/impl/SQLiteImpl.java
+++ src/main/java/tech/goksi/projekatop/persistance/impl/SQLiteImpl.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -110,7 +111,7 @@ public class SQLiteImpl implements DataStorage {
                 }
             }, EncryptionUtils.createHash(password), korisnik.getId());
 
-            return findUserByUsername(korisnik.getUsername()).join();
-        });
+            return findUserByUsername(korisnik.getUsername());
+        }).thenCompose(Function.identity());
     }
 }

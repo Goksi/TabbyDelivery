2413a3ca0df3c34ad19b4a66d38494f07dedadfc
Vukašin Lekić <goksi1312@gmail.com>
2024-01-08 01:23:32 +0100

    Samo cemo otvoriti ceo modul, idgaf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,8 @@
-module tech.goksi.projekatop {
+open module tech.goksi.projekatop {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
 
-
-    opens tech.goksi.projekatop to javafx.fxml;
+    
     exports tech.goksi.projekatop;
-    opens tech.goksi.projekatop.controllers.main to javafx.fxml;
-    opens tech.goksi.projekatop.controllers.nalog to javafx.fxml;
 }

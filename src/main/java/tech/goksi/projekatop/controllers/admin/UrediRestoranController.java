311891c789b89ed5a30fd7563fc62908e07d9363
Vukašin Lekić <goksi1312@gmail.com>
2024-01-22 22:41:20 +0100

    Pikovanje i brisanje logo-a

--- src/main/java/tech/goksi/projekatop/controllers/admin/UrediRestoranController.java
+++ src/main/java/tech/goksi/projekatop/controllers/admin/UrediRestoranController.java
@@ -4,11 +4,10 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Window;
 import tech.goksi.projekatop.models.Jelo;
@@ -32,10 +31,12 @@ public class UrediRestoranController implements DataStorageInjectable {
     @FXML
     private ListView<Jelo> jelaListView;
     private DataStorage storage;
-    private File newLogo;
+    private SimpleObjectProperty<File> newLogoProperty;
+    private boolean logoChanged = false;
 
     public UrediRestoranController() {
         restoranProperty = new SimpleObjectProperty<>();
+        newLogoProperty = new SimpleObjectProperty<>();
     }
 
     public void initialize() {
@@ -62,12 +63,26 @@ public class UrediRestoranController implements DataStorageInjectable {
                 if (newValue.getLogo() == null) {
                     logoView.setImage(ImageUtils.textToImage("Nema\nslike", (int) logoView.getFitWidth(), (int) logoView.getFitHeight()));
                 } else logoView.setImage(newValue.getLogo());
+                logoChanged = true;
             }
+        });
+        newLogoProperty.addListener((obs, oldValue, newValue) -> {
+            if (newValue == null) {
+                logoView.setImage(ImageUtils.textToImage("Nema\nslike", (int) logoView.getFitWidth(), (int) logoView.getFitHeight()));
+            } else logoView.setImage(new Image("file:///" + newValue.getAbsolutePath()));
         });
         jelaListView.sceneProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue != null) {
                 restoranProperty.set((Restoran) newValue.getRoot().getUserData());
             }
+        });
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem obrisiLogo = new MenuItem("Obrisi logo");
+        obrisiLogo.addEventHandler(ActionEvent.ACTION, this::onDeleteLogo);
+        contextMenu.getItems().add(obrisiLogo);
+
+        logoView.setOnContextMenuRequested(contextMenuEvent -> {
+            contextMenu.show(logoView, contextMenuEvent.getScreenX(), contextMenuEvent.getScreenY());
         });
     }
 
@@ -79,12 +94,16 @@ public class UrediRestoranController implements DataStorageInjectable {
     public void onSacuvaj(ActionEvent actionEvent) {
     }
 
+    public void onDeleteLogo(ActionEvent actionEvent) {
+        newLogoProperty.set(null);
+    }
+
     public void onLogoClick(MouseEvent mouseEvent) {
+        if (mouseEvent.getButton() != MouseButton.PRIMARY) return;
         Window parentWindow = ((ImageView) mouseEvent.getSource()).getScene().getWindow();
         File file = ImagePicker.getInstance().open(parentWindow);
         if (file != null) {
-            newLogo = file;
-            logoView.setImage(new Image("file:///" + file.getAbsolutePath()));
+            newLogoProperty.set(file);
         }
     }
 }
